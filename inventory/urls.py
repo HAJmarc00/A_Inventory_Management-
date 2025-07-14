9af52d41b4cf920f07b45aec32035383ce38ac19
Ghazal <hajaceu@gmail.com>
2025-07-14 22:54:49 +0330

    add signup view & form & html

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, SignUp
 
 urlpatterns = [
     path('', index.as_view(), name='index'),
+    path('signup/', SignUp.as_view(), name='signup'),
 ]
