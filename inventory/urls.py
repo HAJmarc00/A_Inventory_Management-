61d547115e4a088856f073c0e1af75e7c69151ab
Donald Trump <hajaceu@gmail.com>
2025-08-08 19:40:29 +0330

    rebuild all project

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, SignUpView
-from django.contrib.auth import views as auth_view
+from .views import Index, SignUpView, Dashboard, AddItem, EditItem, DeleteItem
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', index.as_view(), name='index'),
+    path('', Index.as_view(), name='index'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('add-item/', AddItem.as_view(), name='add-item'),
+    path('edit-item/<int:pk>', EditItem.as_view(), name='edit-item'),
+    path('delete-item/<int:pk>', DeleteItem.as_view(), name='delete-item'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', auth_view.LoginView.as_view(template_name='invetory/login.html'), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='invetory/logout.html'), name='Logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
 ]
